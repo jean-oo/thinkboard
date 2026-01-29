ca51496e78e6ddcb1309dd5ed90fe71a89226b21
Jean <jeanxue@JeandeMacBook-Air.local>
2026-01-29 15:53:16 -0800

    perpared for deplotment

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config();
 //create a ratelimiter that allows 10 req per 20 seconds
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10,"30 s"),
+    limiter: Ratelimit.slidingWindow(20,"20 s"),
 });
 
 export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,19 +4,34 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"; // for getting .env value
 import ratelimiter from "./middleware/rateLimiter.js"
 import cors from "cors"
+import path from "path";
 
 dotenv.config(); // for getting .env value
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware: is a function that runs in the middle between the request and the response. process in the backend
-app.use(cors({origin:"http://localhost:5173"}));
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+      cors({
+        origin: "http://localhost:5173",
+      })
+    );
+  }
 app.use(express.json()); // allows req.body be parsed
 app.use(ratelimiter);
 
 
 app.use("/api/notes", notesRoutes);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 connectDB().then(()=>{
     app.listen(PORT,()=>{
         console.log("Server started on PORT:5001");
